a4c066ec85aa943754819507492383494b989659
spython <emmanuelegbo01@gmail.com>
2023-10-07 05:18:31 +0100

    modified contents of the anouncement jsx and css files

---
+++ src/components/anouncement/Anouncement.jsx
@@ -0,0 +1,7 @@
+import "./anouncement.css"
+
+export default function Anouncement() {
+  return (
+    <div className="announContainer">Super Deal! Free Shipping on Orders over $50</div>
+  )
+}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,10 @@
+import Anouncement from "../../components/anouncement/Anouncement";
 import Navbar from "../../components/navbar/Navbar";
 
 export default function Home() {
   return (
     <div>
+      <Anouncement />
         <Navbar/>
     </div>
   )
