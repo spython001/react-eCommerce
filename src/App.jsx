d59ead10b204c00ba16a1b048c0bb72610939299
spython <emmanuelegbo01@gmail.com>
2023-10-07 21:43:04 +0100

    modified content of the product page jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Home from "./pages/home/Home"
 import ProductList from "./pages/product list/ProductList"
+import Product from "./pages/product/Product"
 
 function App() {
   return (
     <div>
-      <ProductList />
+      <Product />
     </div>
   )
 }
