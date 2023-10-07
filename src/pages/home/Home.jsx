b18e7702f2ce37bdcf595768c56aa711aeed3c35
spython <emmanuelegbo01@gmail.com>
2023-10-07 06:12:38 +0100

    created an assets subfolder in public folder

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Anouncement from "../../components/anouncement/Anouncement";
 import Navbar from "../../components/navbar/Navbar";
+import Slider from "../../components/slider/Slider";
 
 export default function Home() {
   return (
     <div>
       <Anouncement />
         <Navbar/>
+        <Slider/>
     </div>
   )
 }
