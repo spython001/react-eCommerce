6af85c83b1370e639f65e6bd2ee2bcf60923233c
spython <emmanuelegbo01@gmail.com>
2023-10-07 07:24:03 +0100

    created a data jsx files to take some objects

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -7,6 +7,10 @@ const Container = styled.div`
     height: 100vh;
     display: flex;
     position: relative;
+    margin-top: 10px;
+    overflow: hidden;
+    
+    
 `
 
 const Arrow = styled.div`
@@ -27,13 +31,92 @@ const Arrow = styled.div`
     opacity: 0.5;
 `
 
+const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+`
+
+const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items:center;
+    background-color: #${props=>props.bg};
+    
+`
+
+const ImgContainer = styled.div`
+    height: 95%;
+    flex: 1;
+`
+
+const Image = styled.img`
+    height: 80%;
+    
+`
+
+const InfoContainer = styled.div`
+    flex:1;
+    padding: 50px;
+`
+//InfoContainer contents
+const Title = styled.h1`
+    font-size: 70px;
+`
+
+const Desc = styled.p`
+    margin: 50px 0;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
+`
+
 export default function Slider() {
   return (
     <Container>
         <Arrow direction="left">
             <ArrowLeftOutlined/>
         </Arrow>
-        
+        <Wrapper>
+            <Slide bg="f5fafd">
+                <ImgContainer>
+                    <Image src="/assets/comPerson4.png" />
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>SUMMER RUSH!</Title>
+                    <Desc>DONT DULL! GET A FLAT 25% OFF ON NEW ARRIVALS</Desc>
+                    <Button>SHOP NOW</Button>
+                </InfoContainer>
+            </Slide>
+
+            <Slide bg="fcf1ed">
+                <ImgContainer>
+                    <Image src="/assets/comPerson3.png" />
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>WINTER RUSH!</Title>
+                    <Desc>DONT DULL! GET A FLAT 25% OFF ON NEW ARRIVALS</Desc>
+                    <Button>SHOP NOW</Button>
+                </InfoContainer>
+            </Slide>
+
+            <Slide bg="fbf0f4">
+                <ImgContainer>
+                    <Image src="/assets/comPerson4.png" />
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>POPULAR RUSH!</Title>
+                    <Desc>DONT DULL! GET A FLAT 25% OFF ON NEW ARRIVALS</Desc>
+                    <Button>SHOP NOW</Button>
+                </InfoContainer>
+            </Slide>
+        </Wrapper>
         <Arrow direction="right">
             <ArrowRightOutlined/>
         </Arrow>
---
+++ src/data.jsx
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "/assets/comPerson4.png",
+        title: "DONT DULL! GET 25% FLAT ON NEW ARRIVALS",
+        desc: "SUMMER SALE",
+        bg: "f5fafa",
+    },
+    {
+        id: 2,
+        img: "/assets/comPerson3.png",
+        title: "DONT DULL! GET 25% FLAT ON NEW ARRIVALS",
+        desc: "AUTUMN COLLECTION",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "/assets/comPerson3.png",
+        title: "DONT DULL! GET 25% FLAT ON NEW ARRIVALS",
+        desc: "POPULAR SALE",
+        bg: "fbf0f4",
+    }
+]
