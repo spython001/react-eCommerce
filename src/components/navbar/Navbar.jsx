719358a69ab799df6d66918dad9fcd6be515b5e3
spython <emmanuelegbo01@gmail.com>
2023-10-07 03:40:28 +0100

    created sub folders in the page and components folder

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+//import styled from "styled-components"
+import "./navbar.css"
+
+export default function Navbar() {
+  return (
+    <div className="navbarContainer">
+      <div className="navbarWrapper">
+        <div className="navLeft">Left</div>
+        <div className="navCenter">Center</div>
+        <div className="navRight">Right</div>
+      </div>
+    </div>
+  )
+}
