fcbc6f4f4670d12f8fb942518fe112c89c62bc4b
spython <emmanuelegbo01@gmail.com>
2023-10-07 12:09:13 +0100

    modified the contents of newsletter jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Anouncement from "../../components/anouncement/Anouncement";
 import Categories from "../../components/categories/Categories";
 import Navbar from "../../components/navbar/Navbar";
+import Newsletter from "../../components/newsletter/Newsletter";
 import Products from "../../components/products/Products";
 import Slider from "../../components/slider/Slider";
 
@@ -12,6 +13,7 @@ export default function Home() {
         <Slider/>
         <Categories />
         <Products />
+        <Newsletter/>
     </div>
   )
 }
