92d2e9ec48da42616d3399280ff13b433fb4690b
spython <emmanuelegbo01@gmail.com>
2023-10-07 05:10:40 +0100

    modified the navbar jsx and css files

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,8 @@
 //import styled from "styled-components"
 import "./navbar.css"
+import { Search, ShoppingCartOutlined } from "@mui/icons-material"
+import { Badge } from "@mui/material"
+
 
 export default function Navbar() {
   return (
@@ -7,10 +10,23 @@ export default function Navbar() {
       <div className="navbarWrapper">
         <div className="navLeft">
           <span className="navLanguage">EN</span>
-          
+            <div className="navSearch">
+              <input type="text" />
+              <Search style={{ color:"gray", fontSize:16}}/>
+            </div>
         </div>
-        <div className="navCenter">Center</div>
-        <div className="navRight">Right</div>
+        <div className="navCenter">
+          <h1 className="navLogo">SPYTHON.</h1>
+        </div>
+        <div className="navRight">
+          <div className="navMenuItem">REGISTER</div>
+          <div className="navMenuItem">SIGN IN</div>
+          <div className="navMenuItem">
+            <Badge badgeContent={4} color="primary" />
+            <ShoppingCartOutlined />
+        </div>
+        </div>
+        
       </div>
     </div>
   )
