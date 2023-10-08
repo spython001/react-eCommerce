16fd4d0bf7d94cf551685e10cc7f52d73ccc84b5
spython <emmanuelegbo01@gmail.com>
2023-10-08 01:52:19 +0100

    contents of navbar jsx was modified

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,10 +2,12 @@ import styled from "styled-components"
 //import "./navbar.css"
 import { Search, ShoppingCartOutlined } from "@mui/icons-material"
 import { Badge } from "@mui/material"
+import { mobile } from "../../responsive"
 
 
 const Container = styled.div`
   height: 60px;
+  ${mobile({height:"50px"})};
 `
 
 const Wrapper = styled.div`
@@ -13,6 +15,7 @@ const Wrapper = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    ${mobile({padding:"10px 0"})};
 `
 
 const Left = styled.div`
@@ -23,17 +26,20 @@ const Left = styled.div`
 const Language = styled.span`
     font-size: 14px;
     cursor: pointer;
+    ${mobile({display:"none"})};
 `
 const SearchContainer = styled.div`
     border: 0.5px solid lightgray;
     display: flex;
     align-items: center;
     margin-left: 25px;
+    padding: 5px;
     
 `
 const Input = styled.input`
     border: none;
     background-color: #fff;
+    ${mobile({width:"50px"})};
 `
 
 const Center = styled.div`
@@ -42,6 +48,7 @@ const Center = styled.div`
 `
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({fontSize:"20px"})};
 `
 
 const Right = styled.div`
@@ -49,12 +56,14 @@ const Right = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  ${mobile({flex:2, justifyContent:"center"})};
 `
 
 const MenuItem = styled.div`
   font-size: 14px;
     cursor: pointer;
     margin: 25px;
+    ${mobile({fontSize:"12px", marginLeft: "5px"})};
 `
 
 
